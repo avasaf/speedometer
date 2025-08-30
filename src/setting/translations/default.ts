87b9ff7c8974cc95f84e2dd301926dadd52ca5b1
avasaf <114611531+avasaf@users.noreply.github.com>
2025-08-30 20:29:35 +0200

    fix speedometer spacing and settings

--- src/setting/translations/default.ts
+++ src/setting/translations/default.ts
@@ -1,3 +1,11 @@
 export default {
-  verticalAlignment: 'Vertical alignment'
+  verticalAlignment: 'Vertical alignment',
+  speedometer: 'Speedometer',
+  showSpeedometer: 'Show speedometer',
+  gaugeColor: 'Gauge color',
+  needleColor: 'Needle color',
+  textColor: 'Value color',
+  textFont: 'Value font',
+  textSize: 'Value size',
+  textBold: 'Value bold'
 }
